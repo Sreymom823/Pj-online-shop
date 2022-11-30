221ba6d06a185cb9b63e00c48bd02a5383b602ab
Sreymom832 <sreymom.chhum@student.passerellesnumeriques.org>
2022-11-30 20:58:34 +0700

    [Do with Seller Page] do on remove product and add product

--- page/customer/app.js
+++ page/customer/app.js
@@ -1,16 +1,16 @@
 let homePage = document.querySelector(".homePage");
 // console.log(homePage);
-const toBuy = document.querySelector("#buy");
-const dialog_info = document.querySelector("#dialog-buy");
+let toBuy = document.querySelector("#buy");
 
-// let toCreateProduct = document.querySelector("#addProduct");
-// console.log(toCreateProduct);
+let toCancel = document.querySelector("#cancels");
+let order = document.querySelector("#order");
+let dialog_buy = document.querySelector("#dialog-buy");
+let customerInfo = [];
 
 let home = document.querySelector("#home");
 let search = document.querySelector("#search");
 let view = document.querySelector("#view");
-// console.log(homeBtn);
-// console.log(viewBtn);
+
 let page = document.querySelector(".page");
 let dataShow = document.querySelector("#data-show");
 let viewBtn = document.querySelector("#viewBtn");
@@ -25,7 +25,7 @@ function show(element){
     element.style.display = "block";
 };
 
-hide(dialog_info);
+hide(dialog_buy);
 
 home.addEventListener("click", function(){
     hide(searchBtn);
@@ -37,72 +37,412 @@ search.addEventListener("click",function(){
     hide(viewBtn);
     show(searchBtn);
 } );
+view.addEventListener("click", function(){
+    hide(page);
+    hide(searchBtn);
+    show(viewBtn);
+})
 
+toBuy.addEventListener("click", function(){
+    show(dialog_buy);
+})
+toCancel.addEventListener("click", function(){
+    hide(dialog_buy);
+})
+order.addEventListener("click", function(){
+    let NameProducts= document.querySelector("#name-Product").value;
+    let Phones =document.querySelector("#email").value ;
+    let Emails =document.querySelector("#p-number").value;
+    let Locations =document.querySelector("#location").value;
+    let Purchess =document.querySelector("#purches").value;
+    console.log(NameProducts)
+    getInfor ={
+        NameProduct: NameProducts,
+        Phone : Phones,
+        Email : Emails,
+        Location :Locations,
+        Purches :Purchess,
+    }
+    customerInfo.push(getInfor);
+    hide(dialog_buy);
+    console.log(customerInfo);
+});
 
+let information = [
+    {
+        nameProduct:"T-shirt",
+        image:"/img/T-shirt1.webp",
+        Price:"$49",
+        
+    },
+    {
+        nameProduct:"Suit",
+        image:"/img/suit3.jpg",
+        Price:"$15",
 
-// function toSearchPage(event){
-//     hide(page);
-//     hide(viewBtn);
-//     show(searchBtn);
-// };
-// function toHomePage(event){
-//     hide(page);
-//     hide(viewBtn);
-//     show(searchBtn);
-// };
+    },
+    {
+        nameProduct:"Suit",
+        image:"/img/duit4.png",
+        Price:"$15",
 
-// function toViewPage(event){
-//     hide(page);
-//     hide(searchBtn);
-//     show(dataShow)
+    },
+    {
+        nameProduct:"T-shirt",
+        image:"/img/T-shirt1.webp",
+        Price:"$25",
 
-// }
-// // hide(page);
-// // show(page)
+    },
+    {
+        nameProduct:"Dress",
+        image:"/img/dress1.webp",
+        Price:"$36",
 
+    },
+    {
+        nameProduct:"Suit",
+        image:"/img/suit2.webp",
+        Price:"$15",
 
-// // function toBuySomething(){
-// //     show(dialog_info);
-// // }
-// function showdialogTocreate(event){
-//     show(dialog_info)
-// }
+    },
+    {
+        nameProduct:"Dress",
+        image:"/img/dress2.webp",
+        Price:"$15",
 
-// viewBtn.addEventListener("click", toViewPage)
-// homeBtn.addEventListener("click",toHomePage);
-// searchBtn.addEventListener("click", toSearchPage);
-// // toBuy.addEventListener("click", toBuySomething)
-// toCreateProduct.addEventListener("click", showdialogTocreate )
+    },
+    {
+        nameProduct:"Suit",
+        image:"/img/suit1.jpg",
+        Price:"$15",
 
-// // to create and cancel 
-// function toCancel (event){
-//     console.log("cancel")
+    },
+    {
+        nameProduct:"Dress",
+        image:"/img/dress2.webp",
+        Price:"$15",
 
-// }
-// function toCreate (event){
-//     // console.log("create")
-//     let getnamePrduct = document.querySelector("#name-Product").value;
-//     console.log(getnamePrduct);
+    },
+];
 
-//     let getPrice = document.querySelector("#price").value;
-//     console.log(getPrice);
-
-//     let getColors = document.querySelector("#color").value;
-//     console.log(getColors);
-
-//     let getSize = document.querySelector("#size").value;
-//     console.log(getSize);
-
-//     let getpurches = document.querySelector("#purches").value;
-//     console.log(getpurches);
+//  LOCAL STORAGE ---------------------------------------------------------
+function saveInformation() {
+    localStorage.setItem("information", JSON.stringify(information));
+} 
+function loadInformation() {
+    let infoStorage = JSON.parse(localStorage.getItem("information"));
+    if (infoStorage !== null) {
+        information = infoStorage;
+    }
+}
+function rederProduct (event){  
+    // loadInformation();
+    let Data = document.querySelector("#data");
+    Data.remove();
     
-//     let getimg = document.querySelector("#image").value;
-//     console.log(getimg)
-// }
-// let cancelBtn = document.querySelector("#cancel");
-// let createBtn = document.querySelector("#create");
+    Data = document.createElement("div");
+    Data.id = "data";
+    container.appendChild(Data);
+    
 
-// cancelBtn.addEventListener("click", toCancel)
-// createBtn.addEventListener("click", toCreate)
-// console.log(cancelBtn);
-// console.log(createBtn);
+    let namePro = document.createElement("div");
+    namePro.id = "Tshirt";
+
+    let header = document.createElement("div");
+    header.id= "header";
+
+    let btns = document.createElement("button");
+    btns.textContent = "All Product"
+  
+    let allCard = document.createElement("div");
+    allCard.id ="allCard";
+
+    for (let index=0; index < information.length; index++){   
+        let informations=information[index]
+        let groupCard = document.createElement("div");
+        groupCard.id = "groupCard";
+        groupCard.dataset.index = index
+        // console.log(groupCard[i])
+        
+        let img=document.createElement("div");
+        img.id = "img "
+    
+        let imgs = document.createElement("img");
+        imgs.style.height = "350px"
+        imgs.src =  informations.image ;
+        
+        let text = document.createElement("h1");
+        text.textContent = informations.nameProduct;
+
+        let btn = document.createElement("button");
+        btn.id = "forPrice"; 
+        btn.style.width = "100%";
+        btn.style.textAlign = "center";
+
+
+        btn.textContent = informations.Price ;
+        // + ": " + informations.Price ;
+        let icon = document.createElement("div");
+        icon.id = "icon";
+    
+        let imgStar = document.createElement("img");
+        imgStar.className = "star";
+        imgStar.src = "/img/star.webp" ;
+        imgStar.style.width = "15%"
+    
+    
+        // let imgEdit = document.createElement("img");
+        // imgEdit.className = "edite";
+        // imgEdit.src = "/img/edite.png";
+        // imgEdit.style.width ="30px" ;
+        // imgEdit.style.height ="35px" ;
+    
+        icon.appendChild(imgStar);
+        icon.appendChild(imgStar);
+
+        img.appendChild(imgs);
+        img.appendChild(text);
+        img.appendChild(btn);
+        groupCard.appendChild(img);
+        groupCard.appendChild(icon);
+        allCard.appendChild(groupCard);
+    }
+    namePro.appendChild(header);
+    namePro.appendChild(allCard);
+    header.appendChild(btns);
+    // }
+
+
+    Data.appendChild(namePro);
+    // container.appendChild(Data);
+    saveInformation();
+    // loadInformation();
+
+}
+rederProduct();
+
+
+
+
+
+/* to search title */
+
+let btnSearch = document.querySelector("#search-input")
+function searchBar(){
+    let word = btnSearch.value.toUpperCase;
+    let wordInCard = document.querySelector("groupCard").ElementChild[1].toUpperCase;
+    if (word.indexOf (information.length) >-1){
+        wordInCard.style.display = "none";
+    }else {
+        wordInCard.style.display = "Block";
+
+    }
+
+}
+// function myFunction() {
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById("myInput");
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName("li");
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByTagName("a")[0];
+//         txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* create product to show */
+// let information = [
+//     {
+//         nameProduct:"T-shirt",
+//         image:"/img/T-shirt1.webp",
+//         Price:"$49",
+        
+//     },
+//     {
+//         nameProduct:"Suit",
+//         image:"/img/suit3.jpg",
+//         Price:"$15",
+
+//     },
+//     {
+//         nameProduct:"Suit",
+//         image:"/img/duit4.png",
+//         Price:"$15",
+
+//     },
+//     {
+//         nameProduct:"T-shirt",
+//         image:"/img/T-shirt1.webp",
+//         Price:"$25",
+
+//     },
+//     {
+//         nameProduct:"Dress",
+//         image:"/img/dress1.webp",
+//         Price:"$36",
+
+//     },
+//     {
+//         nameProduct:"Suit",
+//         image:"/img/suit2.webp",
+//         Price:"$15",
+
+//     },
+//     {
+//         nameProduct:"Dress",
+//         image:"/img/dress2.webp",
+//         Price:"$15",
+
+//     },
+//     {
+//         nameProduct:"Suit",
+//         image:"/img/suit1.jpg",
+//         Price:"$15",
+
+//     },
+//     {
+//         nameProduct:"Dress",
+//         image:"/img/dress2.webp",
+//         Price:"$15",
+
+//     },
+// ];
+
+// //  LOCAL STORAGE ---------------------------------------------------------
+// function saveInformation() {
+//     localStorage.setItem("information", JSON.stringify(information));
+// } 
+// function loadInformation() {
+//     let infoStorage = JSON.parse(localStorage.getItem("information"));
+//     if (infoStorage !== null) {
+//         information = infoStorage;
+//     }
+// }
+// function rederProduct (event){  
+//     // loadInformation();
+//     let Data = document.querySelector("#data");
+//     Data.remove();
+    
+//     Data = document.createElement("div");
+//     Data.id = "data";
+//     container.appendChild(Data);
+    
+
+//     let namePro = document.createElement("div");
+//     namePro.id = "Tshirt";
+
+//     let header = document.createElement("div");
+//     header.id= "header";
+
+//     let btns = document.createElement("button");
+//     btns.textContent = "All Product"
+  
+//     let allCard = document.createElement("div");
+//     allCard.id ="allCard";
+
+//     for (let index=0; index < information.length; index++){   
+//         let informations=information[index]
+//         let groupCard = document.createElement("div");
+//         groupCard.id = "groupCard";
+//         groupCard.dataset.index = index
+//         // console.log(groupCard[i])
+        
+//         let img=document.createElement("div");
+//         img.id = "img "
+    
+//         let imgs = document.createElement("img");
+//         imgs.style.height = "350px"
+//         imgs.src =  informations.image ;
+        
+//         let text = document.createElement("h1");
+//         text.textContent = informations.nameProduct;
+
+//         let btn = document.createElement("button");
+//         btn.id = "forPrice"; 
+//         btn.style.width = "100%";
+//         btn.style.textAlign = "center";
+
+
+//         btn.textContent = informations.Price ;
+//         // + ": " + informations.Price ;
+//         let icon = document.createElement("div");
+//         icon.id = "icon";
+    
+//         let imgTrush = document.createElement("img");
+//         imgTrush.className = "trush";
+//         imgTrush.src = "/img/trash.png" ;
+//         imgTrush.addEventListener("click", deleteCard)
+    
+//         let imgEdit = document.createElement("img");
+//         imgEdit.className = "edite";
+//         imgEdit.src = "/img/edite.png";
+//         imgEdit.style.width ="30px" ;
+//         imgEdit.style.height ="35px" ;
+    
+//         icon.appendChild(imgTrush);
+//         icon.appendChild(imgEdit);
+
+//         img.appendChild(imgs);
+//         img.appendChild(text);
+//         img.appendChild(btn);
+//         groupCard.appendChild(img);
+//         groupCard.appendChild(icon);
+//         allCard.appendChild(groupCard);
+//     }
+//     namePro.appendChild(header);
+//     namePro.appendChild(allCard);
+//     header.appendChild(btns);
+//     // }
+
+
+//     Data.appendChild(namePro);
+//     // container.appendChild(Data);
+//     hide(dialog_info);
+//     saveInformation();
+//     // loadInformation();
+
+// }
+// rederProduct()
+
+// function toCreate (event){
+//     let getPrice = document.querySelector("#price").value;
+//     let getImg = document.querySelector("#image").value;
+//     let getnamePrduct = document.querySelector("#name-Product").value;
+//     // let informations=information
+//     // getImg.src = informations[i].image
+
+
+//     let newInformation = {
+//         nameProduct:getnamePrduct,
+//         img : getImg,
+//         Price:getPrice,
+//     };
+
+    
+//     information.push(newInformation);
+//     hide(dialog_info);;
+//     loadProduct() 
+//     console.log(information);
+//     // clearDialog();
+//     saveInformation();
+//     // loadInformation();
+
+// }
--- page/seller/app.js
+++ page/seller/app.js
@@ -1,8 +1,8 @@
 let toCreateProduct = document.querySelector("#addProduct");
-const container = document.querySelector("#container")
+let container = document.querySelector("#container");
 // console.log(toCreateProduct);
 
-const dialog_info = document.querySelector("#dialog-info");
+let dialog_info = document.querySelector("#dialog-info");
 // console.log(dialog_info)
 
 function hide(element){
@@ -19,116 +19,6 @@ function showdialogTocreate(){
 }
 
 toCreateProduct.addEventListener("click",showdialogTocreate )
-
-
-
-// function clearDialog(){
-//     let getnamePrduct = document.querySelector("#name-Product");
-//     getnamePrduct.textContent = "";
-//     let getPrice = document.querySelector("#price");
-//     getPrice.textContent = "";
-//     let getColors = document.querySelector("#color");
-//     getColors.textContent = "";
-//     let getSize = document.querySelector("#size");
-//     getSize.textContent = "";
-//     let getpurches = document.querySelector("#purches");
-//     getpurches.textContent = "";
-//     let getimg = document.querySelector("#image");
-//     getimg.textContent = "";
-// }
-
-// let information = [
-//     {
-//         img:"/img/T-shirt.webp",
-//         Price:"$49",
-        
-//     },
-//     {
-//         img:"/img/T-shirt.webp",
-//         Price:"$49",
-
-//     },
-//     {
-//         img:"/img/T-shirt.webp",
-//         Price:"$49",
-
-//     },
-// ] ;
-// function toCreate (event){
-//     let getnamePrduct = document.querySelector("#name-Product").value;
-//     let getPrice = document.querySelector("#price").value;
-//     // let getColors = document.querySelector("#color").value;
-//     // let getSize = document.querySelector("#size").value;
-//     // let getpurches = document.querySelector("#purches").value; 
-//     let getimg = document.querySelector("#image").value;
-//     // for (let index = 0; index < foods.length; index++) {
-//     //     let food = information[index];
-
-//     let Data = document.querySelector("#data");
-
-//     let namePro = document.createElement("div");
-//     namePro.id = "Tshirt";
-//     Data.appendChild(namePro);
-
-//     let header = document.createElement("div");
-//     header.id= "header";
-
-
-//     let btns = document.createElement("button");
-//     btns.textContent = getnamePrduct;
-  
-
-//     let allCard = document.createElement("div");
-//     allCard.id ="allCard";
-
-//     for (let i =0 ;i < information.length; i++){   
-//         let informations=information[i]
-//         let groupCard = document.createElement("div");
-//         groupCard.id = "groupCard";
-//         // groupCard.dataset.i = i;
-        
-//         let img=document.createElement("div");
-//         img.id = "img "
-    
-//         let imgs = document.createElement("img");
-//         imgs.src = informations.img;
-    
-//         let btn = document.createElement("button");
-//         btn.id = "forPrice"; 
-//         btn.textContent = informations.Price;
-    
-//         let icon = document.createElement("div");
-//         icon.id = "icon";
-    
-//         let imgTrush = document.createElement("img");
-//         imgTrush.className = "trush";
-//         imgTrush.src = "/img/trash.png" ;
-    
-//         let imgEdit = document.createElement("img");
-//         imgEdit.className = "edite";
-//         imgEdit.src = "/img/edite.png";
-//         imgEdit.style.width ="30px" ;
-//         imgEdit.style.height ="35px" ;
-    
-//         img.appendChild(imgs);
-//         img.appendChild(btn);
-//         icon.appendChild(imgTrush);
-//         icon.appendChild(imgEdit);
-//         groupCard.appendChild(icon);
-//         groupCard.appendChild(img);
-//         allCard.appendChild(groupCard);
-//     }
-//     namePro.appendChild(allCard);
-//     namePro.appendChild(header);
-//     header.appendChild(btns);
-//     // }
-
-
-//     Data.appendChild(namePro);
-//     clearDialog()
-//     hide(dialog_info);
-
-// }
 
 function clearDialog(){
     let getnamePrduct = document.querySelector("#name-Product");
@@ -194,8 +84,18 @@ let information = [
     },
 ];
 
-function loadProduct (event){
-    
+//  LOCAL STORAGE ---------------------------------------------------------
+function saveInformation() {
+    localStorage.setItem("information", JSON.stringify(information));
+} 
+function loadInformation() {
+    let infoStorage = JSON.parse(localStorage.getItem("information"));
+    if (infoStorage !== null) {
+        information = infoStorage;
+    }
+}
+function rederProduct (event){  
+    // loadInformation();
     let Data = document.querySelector("#data");
     Data.remove();
     
@@ -216,10 +116,12 @@ function loadProduct (event){
     let allCard = document.createElement("div");
     allCard.id ="allCard";
 
-    for (let i =0 ;i < (information.length); i++){   
-        let informations=information[i]
+    for (let index=0; index < information.length; index++){   
+        let informations=information[index]
         let groupCard = document.createElement("div");
         groupCard.id = "groupCard";
+        groupCard.dataset.index = index
+        // console.log(groupCard[i])
         
         let img=document.createElement("div");
         img.id = "img "
@@ -239,7 +141,6 @@ function loadProduct (event){
 
         btn.textContent = informations.Price ;
         // + ": " + informations.Price ;
-        console.log(btn)
         let icon = document.createElement("div");
         icon.id = "icon";
     
@@ -253,6 +154,7 @@ function loadProduct (event){
         imgEdit.src = "/img/edite.png";
         imgEdit.style.width ="30px" ;
         imgEdit.style.height ="35px" ;
+        imgEdit.addEventListener("click", toEiteProduct);
     
         icon.appendChild(imgTrush);
         icon.appendChild(imgEdit);
@@ -271,48 +173,65 @@ function loadProduct (event){
 
 
     Data.appendChild(namePro);
-    // container.appendChild(Data);
+
     hide(dialog_info);
+    // saveInformation();
+
 
 }
-loadProduct()
 
 function toCreate (event){
     let getPrice = document.querySelector("#price").value;
     let getImg = document.querySelector("#image").value;
     let getnamePrduct = document.querySelector("#name-Product").value;
-    // let informations=information
-    // getImg.src = informations[i].image
-
 
     let newInformation = {
         nameProduct:getnamePrduct,
-        img : getImg,
-        // image: "https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?cs=srgb&dl=pexels-pixabay-247502.lpg&fm=jpg",
+        image : getImg,
         Price:getPrice,
     }
-
-    
-    information.push(newInformation)
+    information.push(newInformation);
     hide(dialog_info);
-    loadProduct() 
-    console.log(information)
-    clearDialog()
+    // loadProduct(); 
+    console.log(information);
+    saveInformation();
+    rederProduct();
 
 }
 
 function deleteCard(event) {
-    if (event.target.className == "trush"){
-      event.target.parentElement.parentElement.remove()
-    }
+
+    let index = event.target.parentElement.parentElement.dataset.index;
+    information.splice(index, 1)
+
+    saveInformation();
+    rederProduct();
 }
 
+// loadInformation();
+rederProduct();
 let toEdite = document.querySelector(".edite");
-
 let cancelBtn = document.querySelector("#cancel");
 let createBtn = document.querySelector("#create");
 
 cancelBtn.addEventListener("click", function(){
-    hide(dialog_info)
-})
-createBtn.addEventListener("click", toCreate)
+    hide(dialog_info);
+});
+createBtn.addEventListener("click", toCreate);
+
+
+storeEditInformation = information.length;
+function toEiteProduct(event){
+    createBtn = document.querySelector("#create").textContent = "Edit";
+    let index = event.target.parentElement.parentElement.dataset.index;
+    let informations=information[index]
+    storeEditInformation = index;
+    document.querySelector("#name-Product").value = informations.nameProduct;
+    document.querySelector("#price").value = informations.Price;
+    document.querySelector("#image").value = informations.image;
+    // saveInformation();
+    information.splice(index,1);
+    show(dialog_info);
+}
+
+ 
